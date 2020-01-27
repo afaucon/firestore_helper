410939f9c9ea953c146bd94f5b615f6300d8b4ff
Adrien Faucon <adrien.faucon@gmail.com>
2020-01-27 22:06:08 +0100

    update version

--- firestore_helper/__info__.py
+++ firestore_helper/__info__.py
@@ -1,7 +1,7 @@
 __package_name__ = "firestore_helper"
 __description__ = 'This tools allows to set and get collections from firestore thanks to csv files'
 __url__ = 'https://github.com/afaucon/firestore_helper'
-__version__ = '0.1'
+__version__ = '0.0.1'
 __author__ = 'Adrien Faucon'
 __author_email__ = 'adrien.faucon@gmail.com'
 __license__ = 'MIT License'
